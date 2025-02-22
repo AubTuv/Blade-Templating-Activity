731fcf8b816de6ad731fc217591085f2bfd1acfe
AubTuv <152267051+AubTuv@users.noreply.github.com>
2025-02-22 15:30:36 +0000

    Fixed malformed @foreach statement in explore.blade.php

--- blade-templating-activity/resources/views/explore.blade.php
+++ blade-templating-activity/resources/views/explore.blade.php
@@ -27,8 +27,11 @@
 
     <h3>Using @foreach (Loop through an array)</h3>
 
+    {{-- Debugging: Print the $users variable --}}
+    <pre>{{ var_dump($users ?? 'Users not set') }}</pre>
+
     {{-- Ensure $users is passed from the controller --}}
-    @if(isset($users) && is_array($users) && count($users) > 0)
+    @if(isset($users) && is_array($users))
         @foreach ($users as $user)
             <p>User: {{ $user }}</p>
         @endforeach
@@ -37,6 +40,12 @@
     @endif
 
     <h3>Using @forelse (Handles empty arrays)</h3>
+
+    {{-- Debugging: Print the $products variable --}}
+    <pre>{{ var_dump($products ?? 'Products not set') }}</pre>
+
+    {{-- Ensure $products is defined --}}
+    @php $products = $products ?? []; @endphp
 
     @forelse ($products as $product)
         <p>Product: {{ $product }}</p>
